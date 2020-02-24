a9f84384620fc6a02d868c142ea82e10afee1e7b
Amir BEN SLIMANE <amir.benslimane@gmail.com>
2020-02-24 12:35:26 +0100

    change document tilte

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   const [isReady, setReady] = useState(false);
 
   useEffect(() => {
+    document.title = 'Cat Mash';
     catsService.load().then(() => {
       setReady(true);
     });
