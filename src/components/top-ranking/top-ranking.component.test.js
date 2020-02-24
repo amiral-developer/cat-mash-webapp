f92e92c95e198fc044320f9e8e382b32708db715
Amir BEN SLIMANE <amir.benslimane@gmail.com>
2020-02-24 01:18:02 +0100

    unit test component TopRanking

--- src/components/top-ranking/top-ranking.component.test.js
+++ src/components/top-ranking/top-ranking.component.test.js
@@ -1,23 +1,24 @@
-import React from 'react';
-
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-import TopRanking from './top-ranking.component';
+import React from 'react';
 import { CATS } from '../../assets/data/cats';
 import TopRank from '../top-rank/top-rank.component';
+import TopRanking from './top-ranking.component';
 
 configure({ adapter: new Adapter() });
 
 describe('Component TopRanking', () => {
-    let wrapper;
+    let wrapper = shallow(<div></div>);
 
     beforeEach(() => {
         wrapper = shallow(<TopRanking cats={[]} />);
         wrapper.setProps({ cats: CATS });
     });
 
-    it('should render 3 TopRank components', () => {
+    it('should render 3 first cats', () => {
         expect(wrapper.find(TopRank)).toHaveLength(3);
+        expect(wrapper.find(TopRank).at(0).prop('cat')).toEqual(CATS[0]);
+        expect(wrapper.find(TopRank).at(1).prop('cat')).toEqual(CATS[1]);
+        expect(wrapper.find(TopRank).at(2).prop('cat')).toEqual(CATS[2]);
     });
 });
